5771e5d4bc6a194702379a346f69318923b8bbbd
duarte-gui <55766751+duarte-gui@users.noreply.github.com>
2019-10-27 21:27:27 -0300

    Update bina.py

--- bina.py
+++ bina.py
@@ -2,7 +2,7 @@
 
 #importando libs
 import os
-import time
+from time import sleep
 
 from asterisk.ami import AMIClient
 from asterisk.ami import EventListener
@@ -13,13 +13,23 @@ import requests
 #Executando o cURL com as variáveis binadas
 def event_notification(source, event):
         binado = ( event['ConnectedLineNum'] )
-        os.system('curl -# -3 -X -k -XPOST -H "cookie: Seventh.Auth=eyJuIjoidm9pcCIsImEiOm51bGx9.6tnY42e44PURICRaVFNQWnxo" --header "Content-type: application/json" -d "{accountCode: "s%", zoneCode: 001, eventCode: "s%",  priority: 3,  join: false }" http://10.254.254.72:8080/api/remote-events' % binado)
-        print binado
+        os.system('curl -# -3 -X -k -XPOST -H "cookie: Seventh.Auth=CHAVE_DO_USER_SITUATOR" --header "Content-type: application/json" -d "{accountCode: "s%", zoneCode: 001, eventCode: "s%",  priority: 3,  join: false }" http://10.254.254.72:8080/api/remote-events' % binado)
+        print ("%s" "%s" % binado)
 
-#Login no AMI (Asterisk Manager) e escutando os "novos" eventos "Ringing"
+#Login no AMI (Asterisk Manager) e escutando os "novos" eventos "Ringing", a fila "Exten" e um membro dentro da fila
+#Por enquanto está desse jeito já que a fila gera n eventos que são os membros da fila + a discagem pra esses membros
 client = AMIClient(address="127.0.0.1")
-future = client.login(username="xxxx", secret="xxxxxxxxx")
-client.add_event_listener(EventListener(on_event=event_notification, white_list='Newstate', ChannelStateDesc='Ringing'))
+future = client.login(username="user", secret="password")
+client.add_event_listener(
+        EventListener(
+                on_event=event_notification,
+                white_list='Newstate',
+                ChannelStateDesc='Ringing',
+                Exten='100',
+                CallerIDName='Supervisao Atendimento',
+        )
+)
+
 
 if future.response.is_error():
      raise Exception(str(future.response))
