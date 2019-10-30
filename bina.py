3ffad4de45d196aaceca5eaf4eba2dde693a38af
duarte-gui <55766751+duarte-gui@users.noreply.github.com>
2019-10-29 23:02:26 -0300

    Update bina.py

--- bina.py
+++ bina.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python
 
 #importando libs
-import os
+
 from time import sleep
 
 from asterisk.ami import AMIClient
 from asterisk.ami import EventListener
 
 import requests
+#import os
 
 
-#Executando o cURL com as variáveis binadas
+
 def event_notification(source, event):
-        binado = ( event['ConnectedLineNum'] )
-        os.system('curl -# -3 -X -k -XPOST -H "cookie: Seventh.Auth=CHAVE_DO_USER_SITUATOR" --header "Content-type: application/json" -d "{accountCode: "s%", zoneCode: 001, eventCode: "s%",  priority: 3,  join: false }" http://10.254.254.72:8080/api/remote-events' % (binado, binado))
-        print ("%s" "%s" % (binado, binado))
+    binado = ( event['ConnectedLineNum'] )
+    url = ('http://10.254.254.72:8080/api/remote-events')
+    headers = {
+        'cookie': 'Seventh.Auth=eyJuIjoidGVzdGUiLCJhIjpudWxsfQ.B917GlYR9YIoqEnL4f5plilk',
+        'Content-type': 'application/json',
+    }
+    data = ('{accountCode: "%s", zoneCode: 001, eventCode: "%s",  priority: 3,  join: false}' % (binado, binado))
+    response = requests.post('http://10.254.254.72:8080/api/remote-events', headers=headers, data=data, verify=False)
+    print (data)
 
 #Login no AMI (Asterisk Manager) e escutando os "novos" eventos "Ringing", a fila "Exten" e um membro dentro da fila
-#Por enquanto restringi ao Exten='100' e CallerIDName pra enviar apenas 1 evento, já que a fila gera n eventos que são os membros da fila + a discagem pra esses membros
+#Por enquanto restringi ao Exten='100' e CallerIDName pra enviar apenas 1 evento, ja que a fila gera n eventos que sao os membros da fila + a discagem pra esses membros
+
 client = AMIClient(address="127.0.0.1")
-future = client.login(username="user", secret="password")
-client.add_event_listener(
-        EventListener(
-                on_event=event_notification,
-                white_list='Newstate',
-                ChannelStateDesc='Ringing',
-                Exten='100',
-                CallerIDName='Supervisao Atendimento',
-        )
-)
+future = client.login(username="cxpanel", secret="cxmanager*con")
+client.add_event_listener(EventListener(on_event=event_notification, white_list='Newstate', ChannelStateDesc='Ringing', Exten='100', CallerIDName='Supervisao Atendimento'))
+#client.add_event_listener(EventListener(on_event=event_notification, white_list='Newstate', ChannelStateDesc='Ringing'))
+
 
 
 if future.response.is_error():
@@ -37,6 +39,6 @@ if future.response.is_error():
 
 try:
      while True:
-         time.sleep(10)
+         sleep(10)
 except (KeyboardInterrupt, SystemExit):
      client.logoff()
